a6b1478100d347226e70a65d24744e0ce54bc466
sonia-elear1 <soniadua7913@gmail.com>
2022-11-09 22:41:27 +0530

    adding literals

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(addThreeNumbers(10,20,30));
+    }
+
+    static int addThreeNumbers(int a, int b, int c) {
+        return (a+b+c);
     }
 }
---
+++ src/java_literals.java
@@ -0,0 +1,13 @@
+public class java_literals {
+    public static void main(String[] args) {
+        byte age = 110; // type is byte so value is always between -127 to 128
+        int age1 = 217987;
+        float val = 1.33F; // as default is double so add 'f' or 'F' at last
+        double val1 = 1.34;
+        char ch  = 'S'; // it will contain string with only one character
+        String ch1 = "sona"; // want to add multiple char add as String
+        long value = 34567899857L; // contains L at last for long
+        boolean isTrue = true;
+        System.out.println("literals explained for: -\nbyte,int,float,double,char,long, String and boolean data types ");
+    }
+}
