09db4543148914d4b251ad5134d117cc30d380df
sonia-elear1 <soniadua7913@gmail.com>
2022-11-13 01:36:59 +0530

    operators

---
+++ src/JavaOperators.java
@@ -0,0 +1,40 @@
+public class JavaOperators {
+    public static void main(String[] args) {
+        //Learning operators
+
+        //Arithmetic Operators
+        System.out.println("Arithmatic Operators");
+        System.out.println(2+4 + " is sum of 2 and 4");
+        System.out.println(2-4 + " is difference of 2 and 4");
+        System.out.println(2*4 + " is multiplication of 2 and 4");
+        System.out.println(2/4 + " is division of 2 and 4");
+        System.out.println("\n---------------------\n");
+
+        //Assignment Operators
+        System.out.println("Assignment Operator");
+        int b = 9;
+        b += 3;
+        System.out.println(b);
+        System.out.println("\n---------------------\n");
+
+        //Comparison Operator
+        System.out.println("Comparison Operators");
+        System.out.print("6 == 8 is : " );
+        System.out.println(6==8);
+        System.out.println("\n---------------------\n");
+
+        //Logical Operators
+        System.out.println("Logical Operator");
+        System.out.print("64<5 && 7>4 is : ");
+        System.out.println(64<5 && 7>4);
+        System.out.println("\n---------------------\n");
+
+        //BitWise Operators
+        System.out.println("Bitwise Operator");
+        System.out.print("2&3 is : ");
+        System.out.println(2&3);
+        System.out.println("\n---------------------\n");
+
+
+    }
+}
